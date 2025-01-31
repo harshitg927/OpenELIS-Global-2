2507db9b05901826746184e051ed091aa68207f7
harshith927 <gandhiharshit716@gmail.com>
2025-01-31 22:07:11 +0530

    linked StudyElectronicOrder to the correct side navigation item

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -299,6 +299,12 @@ export default function App() {
                   component={() => <FindOrder />}
                   role="Reception"
                 />
+                <SecureRoute 
+                  path="/StudyElectronicOrders"
+                  exact
+                  component={() => <EOrderPage />}
+                  role="Reception"
+                />
                 <SecureRoute
                   path="/ReportNonConformingEvent"
                   exact
